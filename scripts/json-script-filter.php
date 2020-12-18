40243bc82c4bc7ff9956b8117171686b3421e5fc
Marco Giulietti <ga6t00n@gmail.com>
2020-12-18 12:38:16 +0100

    Last Corrections

--- scripts/json-script-filter.php
+++ scripts/json-script-filter.php
@@ -1,30 +1,36 @@
 <?php
 
+/**
+ * 
+ * Non più utile, tutta la logica è nel json principale. 
+ * 
+ */
+
 // Including Database
-include __DIR__ . '/../partials/database.php';
+// include __DIR__ . '/../partials/database.php';
 
 
-if (empty($_GET) ) {
-    header('Content-Type: application/json');
-    echo json_encode($database);
+// if (empty($_GET) ) {
+//     header('Content-Type: application/json');
+//     echo json_encode($database);
 
-} else {
-    $filter = $_GET['artista'];
+// } else {
+//     $filter = $_GET['artista'];
     
-    if ($filter != 'all') {
-        $dbFiltered = [];
-        foreach ($database as $data) {
-            if (in_array($filter, $data)) {
-                array_push($dbFiltered, $data);   
-            }    
-        }
-        header('Content-Type: application/json');
-        echo json_encode($dbFiltered);
-    } else {
-        header('Content-Type: application/json');
-        echo json_encode($database);
-    }
-}
+//     if ($filter != 'all') {
+//         $dbFiltered = [];
+//         foreach ($database as $data) {
+//             if (in_array($filter, $data)) {
+//                 array_push($dbFiltered, $data);   
+//             }    
+//         }
+//         header('Content-Type: application/json');
+//         echo json_encode($dbFiltered);
+//     } else {
+//         header('Content-Type: application/json');
+//         echo json_encode($database);
+//     }
+// }
 
 
 
